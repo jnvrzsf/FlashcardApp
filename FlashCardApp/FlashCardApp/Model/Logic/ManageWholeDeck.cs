72913a3334c5146ab7fe4955b7f81db1d25f99f7
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-25 20:11:41 +0100

    ManageWholeDeck is created, the Create method is empty!

---
+++ FlashCardApp/FlashCardApp/Model/Logic/ManageWholeDeck.cs
@@ -0,0 +1,24 @@
+﻿using FlashCardApp.Model.Deck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardApp.Model.Logic
+{
+    public class ManageWholeDeck
+    {
+        public ManageWholeDeck()
+        {
+        }
+        
+        public void Create(string path)
+        {
+            //Open the input file and import the lines from it
+            //Split the lines and create Cards
+            //Add these Cards into the WholeDeck
+            //WholeDeck.Instance().Add(new Cards.Card(1, "example", "példa"));
+        }
+    }
+}
