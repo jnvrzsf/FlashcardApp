6211128b61287032eaa3277b1cd0c83523437c94
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-25 20:12:00 +0100

    ManageDeckInProgress is implemented

---
+++ FlashCardApp/FlashCardApp/Model/Logic/ManageDeckInProgress.cs
@@ -0,0 +1,33 @@
+﻿using FlashCardApp.Model.Cards;
+using FlashCardApp.Model.Deck;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardApp.Model.Logic
+{
+    public class ManageDeckInProgress
+    {
+        public ManageDeckInProgress()
+        {
+        }
+
+        public void PickCardsToLearn()
+        {
+            foreach (Card card in WholeDeck.Instance().ListAll())
+            {
+                if (IsHitRateLow(card) && DeckInProgress.Instance().ListAll().Count < 10)
+                {
+                    DeckInProgress.Instance().Add(card);
+                }
+            }
+        }
+
+        public bool IsHitRateLow(Card card)
+        {
+            return !card.HitRateIsGoodEnough;
+        }
+    }
+}
