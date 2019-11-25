a38a64d33a84a463a51bf7e3bf741c92ac1dac62
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-25 20:10:20 +0100

    Deck is implemented as an abstract class

---
+++ FlashCardApp/FlashCardApp/Model/Deck/Deck.cs
@@ -0,0 +1,31 @@
+﻿using FlashCardApp.Model.Cards;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlashCardApp.Model.Deck
+{
+    public abstract class Deck
+    {
+        protected Dictionary<int, Card> cardsInDictionary = new Dictionary<int, Card>();
+
+        public void Add(Card card)
+        {
+            if (cardsInDictionary.ContainsKey(card.ID)) return;
+            cardsInDictionary.Add(card.ID, card);
+        }
+
+        public void Remove(Card card)
+        {
+            cardsInDictionary.Remove(card.ID);
+        }
+
+        public List<Card> ListAll()
+        {
+            return cardsInDictionary.Values.ToList();
+        }
+
+    }
+}
