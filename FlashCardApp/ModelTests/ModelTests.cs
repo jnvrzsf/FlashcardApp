a9f8fd531a3990f611483e4875ded25cd657683f
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-26 17:35:50 +0100

    WholeDeck functions test

--- FlashCardApp/ModelTests/ModelTests.cs
+++ FlashCardApp/ModelTests/ModelTests.cs
@@ -1,6 +1,7 @@
 ﻿
 using System;
 using FlashCardApp.Model.Cards;
+using FlashCardApp.Model.Deck;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ModelTests
@@ -13,6 +14,18 @@ namespace ModelTests
         {
             Card card = new Card(1, "dog", "kutya");
             Assert.AreEqual("dog", card.WordToLearn);
+        }
+
+        [TestMethod]
+        public void CreateWholeDeck()
+        {
+            WholeDeck.Instance().Add(new Card(1, "dog", "kutya"));
+            WholeDeck.Instance().Add(new Card(2, "cat", "macska"));
+            WholeDeck.Instance().Add(new Card(3, "fish", "hal"));
+            WholeDeck.Instance().Add(new Card(4, "lion", "oroszlán"));
+
+            Assert.AreEqual(4, WholeDeck.Instance().ListAll().Count);
+            Assert.AreEqual("macska", WholeDeck.Instance().ListAll()[1].Meaning);
         }
     }
 }
