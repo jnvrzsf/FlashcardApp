5432f99cea9ba45fdcf5df20b6d678a0b6f01a00
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-26 20:45:12 +0100

    ManageDeckInProgress reviews added

--- FlashCardApp/FlashCardApp/Model/Logic/ManageDeckInProgress.cs
+++ FlashCardApp/FlashCardApp/Model/Logic/ManageDeckInProgress.cs
@@ -8,12 +8,16 @@ using System.Threading.Tasks;
 
 namespace FlashCardApp.Model.Logic
 {
+    /* Az aktuálisan tanulandó kártyák kezelése */
     public class ManageDeckInProgress
     {
         public ManageDeckInProgress()
         {
         }
 
+        /* Ellenőrzi az aktuálisan a tanulni valók között lévő kártyákat,
+         * és amennyiben azt az illető már tudja (legalább ötször egymás után eltalálja),
+         akkor azt a kártyát kitörli onnan, és a helyébe egy újat tesz be. */
         public void CheckDeckInProgress()
         {
             foreach (Card card in DeckInProgress.Instance().ListAll())
@@ -26,6 +30,7 @@ namespace FlashCardApp.Model.Logic
             }
         }
 
+        /* Kártyát helyez az aktuálisan tanulandó szavak listájába (max 10) */
         public void PickCardsToLearn()
         {
             foreach (Card card in WholeDeck.Instance().ListAll())
@@ -37,7 +42,8 @@ namespace FlashCardApp.Model.Logic
             }
         }
 
-        public bool IsHitRateLow(Card card)
+        /* Ellenőrzi, hogy az adott kártyát tudja-e már az illető */
+        private bool IsHitRateLow(Card card)
         {
             return !card.AmountOfHitsIsGoodEnough;
         }
