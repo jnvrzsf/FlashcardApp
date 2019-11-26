7ca9cafcaa02dbfc96b23947754bf83f9f8ae1f5
Zsófia Jánvári <janvari.zsofi@gmail.com>
2019-11-26 17:40:01 +0100

    Alap kinézet kész

--- FlashCardApp/FlashCardApp/StudyPage.xaml.cs
+++ FlashCardApp/FlashCardApp/StudyPage.xaml.cs
@@ -20,16 +20,11 @@ namespace FlashCardApp
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DeckSelectionPage : Page
+    public sealed partial class StudyPage : Page
     {
-        public DeckSelectionPage()
+        public StudyPage()
         {
             this.InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(StudyPage));
         }
     }
 }
