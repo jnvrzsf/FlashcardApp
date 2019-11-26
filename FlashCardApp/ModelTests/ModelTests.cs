c8c1fd21b2132f35e25c3e606e1d66d0911ea009
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-26 17:27:28 +0100

    Created a test project for testing models

---
+++ FlashCardApp/ModelTests/ModelTests.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using FlashCardApp.Model.Cards;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelTests
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void CardCreating()
+        {
+            Card card = new Card(1, "dog", "kutya");
+            Assert.AreEqual("dog", card.WordToLearn);
+        }
+    }
+}
