251a6c8d4b957ba37dac0a00ed861e3d28ea6643
Zsófia Jánvári <janvari.zsofi@gmail.com>
2019-11-26 20:32:06 +0100

    láthatóságok binding nélkül megvalósítva

--- FlashCardApp/FlashCardApp/StudyPage.xaml.cs
+++ FlashCardApp/FlashCardApp/StudyPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,14 @@ namespace FlashCardApp
         public StudyPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Rectangle_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            //rectangle.Fill = new SolidColorBrush(Colors.Green);
+            hungarianText.Visibility = Visibility.Visible;
+            wrongBtn.Visibility = Visibility.Visible;
+            rightBtn.Visibility = Visibility.Visible;
         }
     }
 }
