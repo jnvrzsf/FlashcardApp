e9fd01c054aa170e0d374722d09e6848b672c5e5
Roland Fényes <rolandcsabafenyes@edu.bme.hu>
2019-11-26 20:35:39 +0100

    Deck reviews added

--- FlashCardApp/FlashCardApp/Model/Deck/Deck.cs
+++ FlashCardApp/FlashCardApp/Model/Deck/Deck.cs
@@ -7,21 +7,25 @@ using System.Threading.Tasks;
 
 namespace FlashCardApp.Model.Deck
 {
+    /* Absztrakt osztály, a paklik közös funkciói vannak itt */
     public abstract class Deck
     {
         protected Dictionary<int, Card> cardsInDictionary = new Dictionary<int, Card>();
 
+        /* Paklihoz hozzáad */
         public void Add(Card card)
         {
             if (cardsInDictionary.ContainsKey(card.ID)) return;
             cardsInDictionary.Add(card.ID, card);
         }
 
+        /* Elvesz a pakliból */
         public void Remove(Card card)
         {
             cardsInDictionary.Remove(card.ID);
         }
 
+        /* Kilistázás */
         public List<Card> ListAll()
         {
             return cardsInDictionary.Values.ToList();
