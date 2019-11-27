9e84ba0c2d1ae7c50f8e33cd629b694c533c8e17
Mark Olah <mark.olah.om@gmail.com>
2019-11-27 14:39:14 +0100

    Read data from file working. Create method.

--- FlashCardApp/FlashCardApp/Model/Logic/ManageWholeDeck.cs
+++ FlashCardApp/FlashCardApp/Model/Logic/ManageWholeDeck.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FlashCardApp.Model.Logic
 {
@@ -15,10 +16,13 @@ namespace FlashCardApp.Model.Logic
         
         public void Create(string path)
         {
-            //Open the input file and import the lines from it
-            //Split the lines and create Cards
-            //Add these Cards into the WholeDeck
-            //WholeDeck.Instance().Add(new Cards.Card(1, "example", "példa"));
+            StreamReader f = new StreamReader(path);
+            while (!f.EndOfStream)
+            {
+                var line = f.ReadLine();
+                var values = line.Split(',');
+                WholeDeck.Instance().Add(new Cards.Card(Convert.ToInt32(values[0]), values[1], values[2]));
+            }
         }
     }
 }
