d41e03a484a09a0c8fadb2af0282e51ceb60efdc
Mark Olah <mark.olah.om@gmail.com>
2019-11-28 16:31:03 +0100

    FIle picker doesn't work. Restore changes.

--- FlashCardApp/FlashCardApp/Model/Logic/ManageWholeDeck.cs
+++ FlashCardApp/FlashCardApp/Model/Logic/ManageWholeDeck.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+using Windows.Storage.Pickers;
+using Windows.Storage;
 
 namespace FlashCardApp.Model.Logic
 {
