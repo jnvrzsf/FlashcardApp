ca7750f9be0802ccb63b97910eb786c8cf89f653
Mark Olah <mark.olah.om@gmail.com>
2019-11-30 17:41:58 +0100

    ColorToSolidColorBrushConverter implemented.

---
+++ FlashCardApp/FlashCardApp/ViewModel/ColorToSolidColorBrushConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace FlashCardApp.ViewModel
+{
+    public class ColorToSolidColorBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            SolidColorBrush brush = (SolidColorBrush)value;
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
