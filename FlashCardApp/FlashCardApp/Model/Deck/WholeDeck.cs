2b6c5ccbe494b63589c471ddfb822b0c1564f924
Roland Fényes <45495095+rolandfenyes@users.noreply.github.com>
2019-11-30 12:21:50 +0100

    Unnecessary classes commented out

--- FlashCardApp/FlashCardApp/Model/Deck/WholeDeck.cs
+++ FlashCardApp/FlashCardApp/Model/Deck/WholeDeck.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace FlashCardApp.Model.Deck
 {
-    /* Az egész pakli
-     * Lényege, hogy a fájlban tárolt összes szó párt elérhetővé tegye*/
-    public class WholeDeck : Deck
-    {
-        private static WholeDeck instance = new WholeDeck();
-        public static WholeDeck Instance() { return instance; }
-        private WholeDeck() : base()
-        {
-        }
-    }
+    ///* Az egész pakli
+    // * Lényege, hogy a fájlban tárolt összes szó párt elérhetővé tegye*/
+    //public class WholeDeck : Deck
+    //{
+    //    private static WholeDeck instance = new WholeDeck();
+    //    public static WholeDeck Instance() { return instance; }
+    //    private WholeDeck() : base()
+    //    {
+    //    }
+    //}
 }
