8a935b318dc7053e5b16b65bed4d8f9d4124ef99
Roland Fényes <45495095+rolandfenyes@users.noreply.github.com>
2019-11-30 12:55:00 +0100

    ID removed from Card, and initializing the decks is added to App.xaml.cs

--- FlashCardApp/FlashCardApp/Model/Cards/Card.cs
+++ FlashCardApp/FlashCardApp/Model/Cards/Card.cs
@@ -8,16 +8,14 @@ namespace FlashCardApp.Model.Cards
 {
     public class Card
     {
-        public int ID { get; private set; }
         public string WordToLearn { get; private set; }
         public string Meaning { get; private set; }
         
         /* Találatok száma */
         public int HitCount { get; private set; }
 
-        public Card(int id, string wordToLearn, string meaning, int hit)
+        public Card(string wordToLearn, string meaning, int hit)
         {
-            ID = id;
             WordToLearn = wordToLearn;
             Meaning = meaning;
             HitCount = hit;
