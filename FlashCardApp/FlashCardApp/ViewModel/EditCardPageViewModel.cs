05b49d5ac4b122d6d4c13e864018ea1108576512
Roland Fényes <45495095+rolandfenyes@users.noreply.github.com>
2019-11-30 21:03:13 +0100

    EditCardPageViewModel implemented más o menos

--- FlashCardApp/FlashCardApp/ViewModel/EditCardPageViewModel.cs
+++ FlashCardApp/FlashCardApp/ViewModel/EditCardPageViewModel.cs
@@ -1,13 +1,32 @@
-﻿using System;
+﻿using FlashCardApp.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FlashCardApp.ViewModel
 {
-    class EditCardPageViewModel
+    class EditCardPageViewModel :INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private Deck Deck { get; set; }
+        public Card Card { get; set; }
+        public string Meaning => Card.Meaning;
+        public string Word => Card.Word;
 
+        public EditCardPageViewModel(Card card, Deck deck)
+        {
+            Card = card;
+            Deck = deck;
+        }
+
+        private void Save()
+        {
+            //TODO for Zsófi
+            Deck.Save();
+        }
     }
 }
